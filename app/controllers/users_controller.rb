1901b5b83af86b708c2d8b52edc4d68beaa5bc7b
nguyenvandung <dungnguyen16011997@gmail.com>
2020-07-22 16:06:45 +0700

    chapter 7 Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,30 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t ".show.invalid_user_notify"
+    redirect_to root_path
+  end
+
+  def create
+    @user = User.new(user_params) # Not the final implementation!
+    if @user.save
+      flash[:success] = t ".success_notify"
+      redirect_to @user
+    else
+      flash[:danger] = t ".faild_notify"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit User::USERS_PARAMS
+  end
 end
